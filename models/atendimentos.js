95102f95675574669a68c207509276fcdcc4a264
Rafael <tuissirafael@gmail.com>
2021-07-06 22:48:12 -0300

    Adding error validation

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -5,16 +5,46 @@ class Atendimento {
     adiciona(atendimento, res){
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        const atendimentoDatado = {...atendimento, dataCriacao, data}
-        const sql = 'INSERT INTO Atendimentos SET ?'
-        
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-            if(erro){
-                res.status(400).json(erro)
-            } else {
-                res.status(201).json(resultados)
+
+        const validaData = moment(data).isSameOrAfter(dataCriacao)
+        const validaCliente = atendimento.cliente.length >= 5 
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: validaData,
+                mensagem: 'Data deve ser maior ou igual a data atual.'
+            },
+            {
+                nome: 'cliente',
+                valido: validaCliente,
+                mensagem: 'Cliente deve possuir no minímo cinco caracteres'
             }
-        })
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if(existemErros){
+
+            res.status(400).json(erros)
+
+        } else {
+            
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+
+            const sql = 'INSERT INTO Atendimentos SET ?'
+            
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro){
+                    res.status(400).json(erro)
+                } else {
+                    res.status(201).json(resultados)
+                }
+            })
+        }
+
+        
     }
 }
 
